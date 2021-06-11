5786a9a94ddbbe3a003409d03addf930a0d618cb
Hedilton <hediltoncolana@gmail.com>
2021-06-11 15:53:42 -0500

    Agregar archivos de proyecto.

---
+++ EjercicioWinform3Hilos/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace EjercicioWinform3Hilos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.picture2 = new System.Windows.Forms.PictureBox();
+            this.picture = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picture2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(225, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Iniciar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // picture2
+            // 
+            this.picture2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.picture2.Image = global::EjercicioWinform3Hilos.Properties.Resources.el_sol;
+            this.picture2.Location = new System.Drawing.Point(293, 143);
+            this.picture2.Name = "picture2";
+            this.picture2.Size = new System.Drawing.Size(184, 113);
+            this.picture2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picture2.TabIndex = 2;
+            this.picture2.TabStop = false;
+            // 
+            // picture
+            // 
+            this.picture.Image = global::EjercicioWinform3Hilos.Properties.Resources.luna_y_mareas;
+            this.picture.Location = new System.Drawing.Point(45, 143);
+            this.picture.Name = "picture";
+            this.picture.Size = new System.Drawing.Size(161, 113);
+            this.picture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picture.TabIndex = 1;
+            this.picture.TabStop = false;
+            this.picture.UseWaitCursor = true;
+            this.picture.WaitOnLoad = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(111, 263);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(32, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "  ";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(379, 263);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(32, 17);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "  ";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 328);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.picture2);
+            this.Controls.Add(this.picture);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picture2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.PictureBox picture;
+        public System.Windows.Forms.PictureBox picture2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ EjercicioWinform3Hilos/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace EjercicioWinform3Hilos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls =false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread x = new Thread(new ThreadStart(metodo1));
+            Thread y = new Thread(new ThreadStart(metodo2));
+            x.Start();y.Start();
+        }
+        public void metodo1()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Thread.Sleep(3000);
+                this.picture.Visible = false;
+                Thread.Sleep(3000);
+                this.picture.Visible = true;
+
+            }
+            this.checkBox1.Checked = true;
+        }
+        public void metodo2()
+        {
+            for (int y = 1; y <= 5; y++)
+            {
+                Thread.Sleep(3000);
+                this.picture2.Visible = true;
+                Thread.Sleep(3000);
+                this.picture2.Visible = false;
+            }
+            this.picture2.Visible = true;
+            this.checkBox2.Checked = true;
+        }
+    }
+}
